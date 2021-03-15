175c9c2f6ed884962a613d076b0eee4e910e0a00
Greg Scullard <gregscullard@hedera.com>
2021-03-15 12:16:59 +0100

    Default auction duration = 24h

--- hedera-nft-auction-demo-java-node/src/main/java/com/hedera/demo/auction/node/app/EasySetup.java
+++ hedera-nft-auction-demo-java-node/src/main/java/com/hedera/demo/auction/node/app/EasySetup.java
@@ -62,7 +62,7 @@ public class EasySetup {
         auction.put("auctionaccountid", auctionAccount.toString());
         auction.put("reserve", 0);
         auction.put("minimumbid", 10);
-        auction.put("endtimestamp", Instant.now().plus(24, ChronoUnit.DAYS).getEpochSecond());
+        auction.put("endtimestamp", Instant.now().plus(2, ChronoUnit.DAYS).getEpochSecond());
         auction.put("winnercanbid", true);
 
         // store auction data in initDemo.json file

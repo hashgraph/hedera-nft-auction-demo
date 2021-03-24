8237f35e4236743190c46ff9b756b68697d632db
Greg Scullard <gregscullard@hedera.com>
2021-03-24 14:40:44 +0100

    Additional parameters for auction post

--- hedera-nft-auction-demo-java-node/src/main/java/com/hedera/demo/auction/node/app/api/PostAuctionHandler.java
+++ hedera-nft-auction-demo-java-node/src/main/java/com/hedera/demo/auction/node/app/api/PostAuctionHandler.java
@@ -1,5 +1,6 @@
 package com.hedera.demo.auction.node.app.api;
 
+import com.google.errorprone.annotations.Var;
 import com.hedera.demo.auction.node.app.CreateAuction;
 import com.hedera.hashgraph.sdk.PrecheckStatusException;
 import com.hedera.hashgraph.sdk.ReceiptStatusException;
@@ -7,9 +8,13 @@ import io.vertx.core.Handler;
 import io.vertx.core.json.Json;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.RoutingContext;
+import org.jooq.tools.StringUtils;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 public class PostAuctionHandler implements Handler<RoutingContext> {
     public PostAuctionHandler() {
@@ -27,7 +32,26 @@ public class PostAuctionHandler implements Handler<RoutingContext> {
         var data = body.mapTo(RequestCreateAuction.class);
 
         try {
-            CreateAuction.create(data.auctionFile, "");
+            @Var String fileName = data.auctionFile;
+
+            if (StringUtils.isEmpty(fileName)) {
+                fileName = "./sample-files/initDemo.json";
+
+                JsonObject auction = new JsonObject();
+                auction.put("tokenid", data.tokenid);
+                auction.put("auctionaccountid", data.auctionaccountid);
+                auction.put("reserve", data.reserve);
+                auction.put("minimumbid", data.minimumbid);
+                auction.put("endtimestamp", data.endtimestamp);
+                auction.put("winnercanbid", data.winnercanbid);
+
+                // store auction data in initDemo.json file
+                FileWriter myWriter = new FileWriter(fileName, UTF_8);
+                myWriter.write(auction.encodePrettily());
+                myWriter.close();
+            }
+
+            CreateAuction.create(fileName, "");
 
             JsonObject response = new JsonObject();
             response.put("status", "created");
--- hedera-nft-auction-demo-java-node/src/main/java/com/hedera/demo/auction/node/app/api/RequestCreateAuction.java
+++ hedera-nft-auction-demo-java-node/src/main/java/com/hedera/demo/auction/node/app/api/RequestCreateAuction.java
@@ -3,4 +3,10 @@ package com.hedera.demo.auction.node.app.api;
 @SuppressWarnings("unused")
 public class RequestCreateAuction {
     public String auctionFile = "";
+    public String tokenid = "";
+    public String auctionaccountid = "";
+    public long reserve = 0L;
+    public long minimumbid = 0L;
+    public String endtimestamp = "";
+    public boolean winnercanbid = false;
 }

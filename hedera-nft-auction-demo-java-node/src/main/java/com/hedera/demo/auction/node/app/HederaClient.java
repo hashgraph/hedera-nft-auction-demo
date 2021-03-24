c29b4355f712905c767f2fdc669146059dd75552
Greg Scullard <gregscullard@hedera.com>
2021-03-24 14:42:19 +0100

    simplify

--- hedera-nft-auction-demo-java-node/src/main/java/com/hedera/demo/auction/node/app/HederaClient.java
+++ hedera-nft-auction-demo-java-node/src/main/java/com/hedera/demo/auction/node/app/HederaClient.java
@@ -1,12 +1,12 @@
 package com.hedera.demo.auction.node.app;
 
-import com.google.errorprone.annotations.Var;
 import com.hedera.hashgraph.sdk.AccountId;
 import com.hedera.hashgraph.sdk.Client;
 import com.hedera.hashgraph.sdk.PrivateKey;
 import com.hedera.hashgraph.sdk.PublicKey;
 import io.github.cdimascio.dotenv.Dotenv;
 import lombok.extern.log4j.Log4j2;
+import org.jooq.tools.StringUtils;
 
 import java.util.List;
 import java.util.Objects;
@@ -18,7 +18,7 @@ public class HederaClient {
     private static final AccountId OPERATOR_ID = AccountId.fromString(Objects.requireNonNull(env.get("OPERATOR_ID")));
     private static final PrivateKey OPERATOR_KEY = PrivateKey.fromString(Objects.requireNonNull(env.get("OPERATOR_KEY")));
     private static final String VUE_APP_NETWORK = Optional.ofNullable(env.get("VUE_APP_NETWORK").toUpperCase()).orElse("");
-    private static final String MIRROR_PROVIDER = Optional.ofNullable(env.get("MIRROR_PROVIDER")).orElse("kabuto").toUpperCase();
+    private static final String MIRROR_PROVIDER = Optional.ofNullable(env.get("MIRROR_PROVIDER").toUpperCase()).orElse("kabuto").toUpperCase();
 
     private HederaClient() {
     }
@@ -33,52 +33,17 @@ public class HederaClient {
     }
     public static String getMirrorProvider() {return MIRROR_PROVIDER;}
     public static String getMirrorUrl() throws Exception {
-        @Var String url = "";
-        switch (VUE_APP_NETWORK) {
-            case "PREVIEWNET":
-                switch (MIRROR_PROVIDER) {
-                    case "DRAGONGLASS":
-                        //TODO: Dragonglass API endpoint
-                        break;
-                    default:
-                        url = "previewnet.mirrornode.hedera.com";
-                        break;
-                }
-                break;
-            case "TESTNET":
-                switch (MIRROR_PROVIDER) {
-                    case "HEDERA":
-                        url = "testnet.mirrornode.hedera.com";
-                        break;
-                    case "DRAGONGLASS":
-                        //TODO: Dragonglass API endpoint
-                        break;
-                    default:
-                        url = "api.testnet.kabuto.sh/v1/";
-                        break;
-                }
-                break;
-            case "MAINNET":
-                switch (MIRROR_PROVIDER) {
-                    case "HEDERA":
-                        url = "mainnet.mirrornode.hedera.com";
-                        break;
-                    case "DRAGONGLASS":
-                        //TODO: Dragonglass API endpoint
-                        break;
-                    default:
-                        url = "api.kabuto.sh/v1";
-                        break;
-                }
-                break;
-            default:
-                throw new Exception("VUE_APP_NETWORK environment variable not set");
-
+        String envVariable = "REST_".concat(MIRROR_PROVIDER).concat("_")
+                .concat(VUE_APP_NETWORK);
+        String url = env.get(envVariable);
+        if (StringUtils.isBlank(url)) {
+            throw new Exception("VUE_APP_NETWORK and/or MIRROR_PROVIDER environment variables not set");
         }
         return url;
     }
+
     public static Client getClient() throws Exception {
-        @Var Client client = Client.forTestnet();
+        Client client;
 
         switch (VUE_APP_NETWORK) {
             case "PREVIEWNET":
@@ -86,22 +51,23 @@ public class HederaClient {
                 break;
             case "TESTNET":
                 client = Client.forTestnet();
-                if ( ! MIRROR_PROVIDER.equals("HEDERA")) {
-                    // the SDK doesn't support mirror subscriptions to dragonglass, default to kabuto
-                    client.setMirrorNetwork(List.of("api.testnet.kabuto.sh:50211"));
-                }
                 break;
             case "MAINNET":
                 client = Client.forMainnet();
-                if ( ! MIRROR_PROVIDER.equals("HEDERA")) {
-                    // the SDK doesn't support mirror subscriptions to dragonglass, default to kabuto
-                    client.setMirrorNetwork(List.of("api.kabuto.sh:50211"));
-                }
                 break;
             default:
                 log.error(".env configuration missing.");
                 throw new Exception("VUE_APP_NETWORK environment variable not set");
         }
+
+        String envVariable = "GRPC_".concat(MIRROR_PROVIDER).concat("_")
+                .concat(VUE_APP_NETWORK);
+        String url = env.get(envVariable);
+        if (StringUtils.isBlank(url)) {
+            throw new Exception("VUE_APP_NETWORK and/or MIRROR_PROVIDER environment variables not set");
+        }
+
+        client.setMirrorNetwork(List.of(url));
 
         client.setOperator(OPERATOR_ID, OPERATOR_KEY);
 

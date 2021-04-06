e85e105d023fcb7760dd9316cfd16f6948f8d053
Greg Scullard <gregscullard@hedera.com>
2021-04-06 18:09:56 +0200

    Adds 3 test tasks to gradle, unit, integration, system + starter unit tests on mirror mapping

---
+++ hedera-nft-auction-demo-java-node/src/test/java/com/hedera/demo/auction/node/test/unit/mirrormapping/SampleUnitTest.java
@@ -0,0 +1,12 @@
+package com.hedera.demo.auction.node.test.unit.mirrormapping;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SampleUnitTest {
+    @Test
+    public void testUnit() {
+        assertEquals("Unit", "Unit");
+    }
+}

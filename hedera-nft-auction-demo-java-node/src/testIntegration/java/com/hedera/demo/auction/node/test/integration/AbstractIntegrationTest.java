e3ec2604cefd4c67e19e421623079312685e81d6
Greg Scullard <gregscullard@hedera.com>
2021-04-09 20:02:12 +0200

    Refactor to merge auction and bid test support + api verticle support

---
+++ hedera-nft-auction-demo-java-node/src/testIntegration/java/com/hedera/demo/auction/node/test/integration/AbstractIntegrationTest.java
@@ -0,0 +1,220 @@
+package com.hedera.demo.auction.node.test.integration;
+
+import com.hedera.demo.auction.node.app.domain.Auction;
+import com.hedera.demo.auction.node.app.domain.Bid;
+import io.github.cdimascio.dotenv.Dotenv;
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.client.WebClient;
+import io.vertx.ext.web.client.WebClientOptions;
+import io.vertx.junit5.VertxExtension;
+import org.flywaydb.core.Flyway;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(VertxExtension.class)
+public class AbstractIntegrationTest {
+
+    private final WebClientOptions webClientOptions = new WebClientOptions()
+            .setUserAgent("HederaAuction/1.0")
+            .setKeepAlive(false);
+    protected WebClient webClient = WebClient.create(Vertx.vertx(), webClientOptions);
+    protected final static Dotenv env = Dotenv.configure().filename(".env.integration").ignoreIfMissing().load();
+
+    private int index;
+
+    private String stringPlusIndex(String string) {
+        return string.concat(String.valueOf(this.index));
+    }
+
+    long winningBid() {
+        return 30L + this.index;
+    }
+    String winningAccount() {
+        return stringPlusIndex("winningAccount");
+    }
+    String winningTimestamp() {
+        return stringPlusIndex("winningTimestamp");
+    }
+    String endtimestamp() {
+        return stringPlusIndex("endtimestamp");
+    }
+    long reserve() {
+        return 20L + this.index;
+    }
+    String status() {
+        return stringPlusIndex("endtimestamp");
+    }
+    boolean winnercanbid() {
+        return (this.index == 1);
+    }
+    String winningtxid() {
+        return stringPlusIndex("endtimestamp");
+    }
+    String winningtxhash() {
+        return stringPlusIndex("endtimestamp");
+    }
+    String tokenimage() {
+        return stringPlusIndex("endtimestamp");
+    }
+    long minimumbid() {
+        return 10L + this.index;
+    }
+    String starttimestamp() {
+        return stringPlusIndex("endtimestamp");
+    }
+    String transfertxid() {
+        return stringPlusIndex("endtimestamp");
+    }
+    String transfertxhash() {
+        return stringPlusIndex("endtimestamp");
+    }
+    String lastConsensusTimestamp() {
+        return stringPlusIndex("endtimestamp");
+    }
+    String auctionAccountId() {
+        return stringPlusIndex("endtimestamp");
+    }
+    String tokenId() {
+        return stringPlusIndex("endtimestamp");
+    }
+
+    public String timestamp() {
+        return stringPlusIndex("timestamp");
+    }
+    String bidderaccountid() {
+        return stringPlusIndex("bidderaccountid");
+    }
+    Long bidamount() {
+        return 30L + this.index;
+    }
+    boolean refunded() {
+        return (this.index == 1);
+    }
+    String refundtxid() {
+        return stringPlusIndex("refundtxid");
+    }
+    String refundtxhash() {
+        return stringPlusIndex("refundtxhash");
+    }
+    String transactionid() {
+        return stringPlusIndex("transactionid");
+    }
+    String transactionhash() {
+        return stringPlusIndex("transactionhash");
+    }
+
+    protected Auction testAuctionObject(int index) {
+        this.index = index;
+        Auction auction = new Auction();
+
+        auction.setWinningbid(winningBid());
+        auction.setWinningaccount(winningAccount());
+        auction.setWinningtimestamp(winningTimestamp());
+        auction.setTokenid(tokenId());
+        auction.setAuctionaccountid(auctionAccountId());
+        auction.setEndtimestamp(endtimestamp());
+        auction.setReserve(reserve());
+        auction.setStatus(status());
+        auction.setWinnercanbid(winnercanbid());
+        auction.setWinningtxid(winningtxid());
+        auction.setWinningtxhash(winningtxhash());
+        auction.setTokenimage(tokenimage());
+        auction.setMinimumbid(minimumbid());
+        auction.setStarttimestamp(starttimestamp());
+        auction.setTransfertxid(transfertxid());
+        auction.setTransfertxhash(transfertxhash());
+        auction.setLastconsensustimestamp(lastConsensusTimestamp());
+
+        return auction;
+    }
+
+    public void testNewAuction(Auction auction, Auction getAuction) {
+        assertEquals(auction.getTokenid(),getAuction.getTokenid());
+        assertEquals("0.0",getAuction.getLastconsensustimestamp());
+        assertEquals(auction.getAuctionaccountid(),getAuction.getAuctionaccountid());
+        assertEquals(auction.getEndtimestamp(),getAuction.getEndtimestamp());
+        assertEquals(auction.getReserve(),getAuction.getReserve());
+        assertEquals(winnercanbid(), getAuction.getWinnerCanBid());
+        assertEquals(auction.getTokenimage(),getAuction.getTokenimage());
+        assertEquals(auction.getWinningbid(),getAuction.getWinningbid());
+        assertEquals(auction.getMinimumbid(),getAuction.getMinimumbid());
+    }
+
+    protected Bid testBidObject(int index, int auctionId) {
+        this.index = index;
+        Bid bid = new Bid();
+
+        bid.setTimestamp(timestamp());
+        bid.setAuctionid(auctionId);
+        bid.setBidderaccountid(bidderaccountid());
+        bid.setBidamount(bidamount());
+        bid.setStatus(status());
+        bid.setRefunded(refunded());
+        bid.setRefundtxid(refundtxid());
+        bid.setRefundtxhash(refundtxhash());
+        bid.setTransactionid(transactionid());
+        bid.setTransactionhash(transactionhash());
+
+        return bid;
+    }
+
+
+    public void testNewBid(Bid bid, Bid newBid) {
+        assertEquals(bid.getAuctionid(),newBid.getAuctionid());
+        assertEquals(bid.getStatus(),newBid.getStatus());
+        assertEquals(bid.getTimestamp(),newBid.getTimestamp());
+        assertEquals(bid.getBidamount(),newBid.getBidamount());
+        assertEquals(bid.getBidderaccountid(),newBid.getBidderaccountid());
+        assertEquals(bid.getTransactionid(),newBid.getTransactionid());
+        assertEquals(bid.getTransactionhash(),newBid.getTransactionhash());
+    }
+
+    public void verifyBidContents(Bid bid, int auctionId) {
+        assertEquals(timestamp(), bid.getTimestamp());
+        assertEquals(auctionId, bid.getAuctionid());
+        assertEquals(bidderaccountid(), bid.getBidderaccountid());
+        assertEquals(bidamount(), bid.getBidamount());
+        assertEquals(status(), bid.getStatus());
+        assertEquals(refunded(), bid.getRefunded());
+        assertEquals(refundtxid(), bid.getRefundtxid());
+        assertEquals(refundtxhash(), bid.getRefundtxhash());
+        assertEquals(transactionid(), bid.getTransactionid());
+        assertEquals(transactionhash(), bid.getTransactionhash());
+    }
+
+    protected void migrate(PostgreSQLContainer postgres) {
+        String postgresUrl = postgres.getJdbcUrl();
+        String postgresUser = postgres.getUsername();
+        String postgresPassword = postgres.getPassword();
+        Flyway flyway = Flyway
+                .configure()
+                .dataSource(postgresUrl, postgresUser, postgresPassword)
+                .locations("filesystem:./src/main/resources/migrations")
+                .load();
+        flyway.migrate();
+
+    }
+
+    protected DeploymentOptions getVerticleDeploymentOptions(String databaseUrl, String username, String password) throws IOException {
+//        ServerSocket socket = new ServerSocket(0);
+//        int port = socket.getLocalPort();
+//        socket.close();
+
+        JsonObject config = new JsonObject()
+                .put("envFile",".env")
+                .put("envPath",".")
+                .put("DATABASE_URL",databaseUrl.replace("jdbc:",""))
+                .put("DATABASE_USERNAME", username)
+                .put("DATABASE_PASSWORD", password)
+                .put("POOL_SIZE", "1")
+                .put("VUE_APP_API_PORT","9005");
+        DeploymentOptions options = new DeploymentOptions().setConfig(config).setInstances(1);
+        return options;
+    }
+}

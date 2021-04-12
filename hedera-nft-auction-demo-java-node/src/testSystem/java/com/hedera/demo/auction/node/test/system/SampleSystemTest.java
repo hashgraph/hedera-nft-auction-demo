6ac31fa94c071b0c19bf4f6965b802ed539076b6
Greg Scullard <gregscullard@hedera.com>
2021-04-12 16:11:39 +0200

    Create SampleSystemTest.java

---
+++ hedera-nft-auction-demo-java-node/src/testSystem/java/com/hedera/demo/auction/node/test/system/SampleSystemTest.java
@@ -0,0 +1,12 @@
+package com.hedera.demo.auction.node.test.system;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SampleSystemTest {
+    @Test
+    public void testSystem() {
+        assertEquals("System", "System");
+    }
+}

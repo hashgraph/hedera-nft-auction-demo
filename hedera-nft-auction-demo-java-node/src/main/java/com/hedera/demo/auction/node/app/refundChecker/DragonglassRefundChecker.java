2d3dea2ac251bdd6168fae60efcd4c2c311c704d
Greg Scullard <gregscullard@hedera.com>
2021-04-14 12:17:56 +0200

    Pass mirrorQueryFrequency as parameter rather than env

--- hedera-nft-auction-demo-java-node/src/main/java/com/hedera/demo/auction/node/app/refundChecker/DragonglassRefundChecker.java
+++ hedera-nft-auction-demo-java-node/src/main/java/com/hedera/demo/auction/node/app/refundChecker/DragonglassRefundChecker.java
@@ -2,15 +2,14 @@ package com.hedera.demo.auction.node.app.refundChecker;
 
 import com.hedera.demo.auction.node.app.HederaClient;
 import com.hedera.demo.auction.node.app.repository.BidsRepository;
-import io.github.cdimascio.dotenv.Dotenv;
 import io.vertx.ext.web.client.WebClient;
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2
 public class DragonglassRefundChecker extends AbstractRefundChecker implements RefundCheckerInterface {
 
-    public DragonglassRefundChecker(HederaClient hederaClient, WebClient webClient, BidsRepository bidsRepository, Dotenv env) {
-        super(hederaClient, webClient, bidsRepository, env);
+    public DragonglassRefundChecker(HederaClient hederaClient, WebClient webClient, BidsRepository bidsRepository, int mirrorQueryFrequency) {
+        super(hederaClient, webClient, bidsRepository, mirrorQueryFrequency);
     }
 
     @Override

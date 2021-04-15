e94f023cd4ef687783f3ae2c861210d2ba532142
Greg Scullard <gregscullard@hedera.com>
2021-04-15 12:36:06 +0200

    Wrapper for mirror topic message to help testing

---
+++ hedera-nft-auction-demo-java-node/src/main/java/com/hedera/demo/auction/node/app/subscriber/TopicMessageWrapper.java
@@ -0,0 +1,32 @@
+package com.hedera.demo.auction.node.app.subscriber;
+
+import com.hedera.hashgraph.sdk.TopicMessage;
+import com.hedera.hashgraph.sdk.TransactionId;
+
+import javax.annotation.Nullable;
+import java.time.Instant;
+
+public class TopicMessageWrapper {
+    public final Instant consensusTimestamp;
+    public final byte[] contents;
+    public final byte[] runningHash;
+    public final long sequenceNumber;
+    @Nullable
+    public final TransactionId transactionId;
+
+    TopicMessageWrapper(TopicMessage topicMessage) {
+        this.consensusTimestamp = topicMessage.consensusTimestamp;
+        this.contents = topicMessage.contents;
+        this.runningHash = topicMessage.runningHash;
+        this.sequenceNumber = topicMessage.sequenceNumber;
+        this.transactionId = topicMessage.transactionId;
+    }
+    public TopicMessageWrapper(Instant consensusTimestamp, byte[] contents, byte[] runningHash, long sequenceNumber, @Nullable TransactionId transactionId) {
+        this.consensusTimestamp = consensusTimestamp;
+        this.contents = contents;
+        this.runningHash = runningHash;
+        this.sequenceNumber = sequenceNumber;
+        this.transactionId = transactionId;
+    }
+
+}

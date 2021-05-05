daca800d89802d6112c04d8d81f7fa36eace1544
Greg Scullard <gregscullard@hedera.com>
2021-05-05 23:20:30 +0200

    Error handling

--- hedera-nft-auction-demo-java-node/src/main/java/com/hedera/demo/auction/node/app/bidwatcher/HederaBidsWatcher.java
+++ hedera-nft-auction-demo-java-node/src/main/java/com/hedera/demo/auction/node/app/bidwatcher/HederaBidsWatcher.java
@@ -1,6 +1,7 @@
 package com.hedera.demo.auction.node.app.bidwatcher;
 
 import com.hedera.demo.auction.node.app.HederaClient;
+import com.hedera.demo.auction.node.app.domain.Auction;
 import com.hedera.demo.auction.node.app.repository.AuctionsRepository;
 import com.hedera.demo.auction.node.app.repository.BidsRepository;
 import io.vertx.core.json.JsonObject;
@@ -13,59 +14,67 @@ import java.util.concurrent.atomic.AtomicBoolean;
 @Log4j2
 public class HederaBidsWatcher extends AbstractBidsWatcher implements BidsWatcherInterface {
 
-    public HederaBidsWatcher(HederaClient hederaClient, WebClient webClient, AuctionsRepository auctionsRepository, BidsRepository bidsRepository, int auctionId, String refundKey, int mirrorQueryFrequency) throws Exception {
+    // private auction object so that we don't interfere with the abstract instance
+    private Auction watchedAuction;
+
+    public HederaBidsWatcher(HederaClient hederaClient, WebClient webClient, AuctionsRepository auctionsRepository, BidsRepository bidsRepository, int auctionId, String refundKey, int mirrorQueryFrequency) {
         super(hederaClient, webClient, auctionsRepository, bidsRepository, auctionId, refundKey, mirrorQueryFrequency);
     }
 
     @Override
-    public void watch() throws Exception {
+    public void watch() {
 
         AtomicBoolean querying = new AtomicBoolean(false);
 
-        log.info("Watching auction account Id " + auction.getAuctionaccountid() + ", token Id " + auction.getTokenid());
+        try {
+            watchedAuction = auctionsRepository.getAuction(auctionId);
+            log.info("Watching auction account Id " + watchedAuction.getAuctionaccountid() + ", token Id " + watchedAuction.getTokenid());
 
-        var webQuery = webClient
-                .get(mirrorURL, "/api/v1/transactions")
-                .addQueryParam("account.id", auction.getAuctionaccountid())
-                .addQueryParam("transactiontype", "CRYPTOTRANSFER")
-                .addQueryParam("order", "asc")
-                .addQueryParam("timestamp","gt:0");
+            var webQuery = webClient
+                    .get(mirrorURL, "/api/v1/transactions")
+                    .addQueryParam("account.id", auction.getAuctionaccountid())
+                    .addQueryParam("transactiontype", "CRYPTOTRANSFER")
+                    .addQueryParam("order", "asc")
+                    .addQueryParam("timestamp", "gt:0");
 
-        while (runThread) {
-            if (!querying.get()) {
-                querying.set(true);
+            while (runThread) {
+                // reload auction from database
+                watchedAuction = auctionsRepository.getAuction(auctionId);
+                if (!querying.get()) {
+                    querying.set(true);
 
-                log.debug("Checking for bids on account " + auction.getAuctionaccountid() + " and token " + auction.getTokenid());
+                    log.debug("Checking for bids on account " + watchedAuction.getAuctionaccountid() + " and token " + watchedAuction.getTokenid());
 
-                if (auction.getLastconsensustimestamp() != null) {
-                    webQuery.setQueryParam("timestamp", "gt:".concat(auction.getLastconsensustimestamp()));
-                }
-
-                webQuery.as(BodyCodec.jsonObject())
-                .send(response -> {
-                    if (response.succeeded()) {
-                        JsonObject body = response.result().body();
-                        try {
-                            handleResponse(body);
-                        } catch (RuntimeException e) {
-                            log.error(e);
-                        } finally {
-                            querying.set(false);
-                        }
-                    } else {
-                        log.error(response.cause().getMessage());
-                        querying.set(false);
+                    if (watchedAuction.getLastconsensustimestamp() != null) {
+                        webQuery.setQueryParam("timestamp", "gt:".concat(watchedAuction.getLastconsensustimestamp()));
                     }
-                });
+
+                    webQuery.as(BodyCodec.jsonObject())
+                            .send(response -> {
+                                if (response.succeeded()) {
+                                    JsonObject body = response.result().body();
+                                    try {
+                                        handleResponse(body);
+                                    } catch (RuntimeException e) {
+                                        log.error(e);
+                                    } finally {
+                                        querying.set(false);
+                                    }
+                                } else {
+                                    log.error(response.cause().getMessage());
+                                    querying.set(false);
+                                }
+                            });
+                }
             }
-            try {
-                Thread.sleep(this.mirrorQueryFrequency);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-                log.error(e);
-            }
-            // reload auction from database
-            this.auction = auctionsRepository.getAuction(auctionId);
+        } catch (Exception e) {
+            log.error(e);
+        }
+        try {
+            Thread.sleep(this.mirrorQueryFrequency);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            log.error(e);
         }
     }
 }

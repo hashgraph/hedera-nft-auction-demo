1f7c82c2e7125e4c03739b0973268871d69d116c
Greg Scullard <gregscullard@hedera.com>
2021-05-18 13:50:33 +0200

    Tidy imports

--- hedera-nft-auction-demo-java-node/src/main/java/com/hedera/demo/auction/node/app/closurewatcher/AbstractAuctionsClosureWatcher.java
+++ hedera-nft-auction-demo-java-node/src/main/java/com/hedera/demo/auction/node/app/closurewatcher/AbstractAuctionsClosureWatcher.java
@@ -1,10 +1,17 @@
 package com.hedera.demo.auction.node.app.closurewatcher;
 
+import com.google.errorprone.annotations.Var;
 import com.hedera.demo.auction.node.app.HederaClient;
 import com.hedera.demo.auction.node.app.domain.Auction;
 import com.hedera.demo.auction.node.app.mirrormapping.MirrorTransactions;
 import com.hedera.demo.auction.node.app.repository.AuctionsRepository;
-import com.hedera.hashgraph.sdk.*;
+import com.hedera.hashgraph.sdk.AccountId;
+import com.hedera.hashgraph.sdk.AccountUpdateTransaction;
+import com.hedera.hashgraph.sdk.Client;
+import com.hedera.hashgraph.sdk.PrivateKey;
+import com.hedera.hashgraph.sdk.Status;
+import com.hedera.hashgraph.sdk.TransactionReceipt;
+import com.hedera.hashgraph.sdk.TransactionResponse;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.client.WebClient;
 import lombok.extern.log4j.Log4j2;
@@ -91,7 +98,7 @@ public abstract class AbstractAuctionsClosureWatcher {
     }
 
     protected void setSignatureRequiredOnAuctionAccount(int auctionId) {
-        Auction auction = null;
+        @Var Auction auction = null;
         try {
             auction = auctionsRepository.getAuction(auctionId);
         } catch (Exception e) {

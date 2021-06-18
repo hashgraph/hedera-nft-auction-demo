00e656372b7ee10de4ea1b4697a630f5cf2481a5
Keegan Thompson <whereiskeegan@gmail.com>
2021-06-18 15:33:05 -0700

    fixing bid button

--- hedera-nft-auction-demo-javascript-client/components/Auction/RightColumn/BidButton.js
+++ hedera-nft-auction-demo-javascript-client/components/Auction/RightColumn/BidButton.js
@@ -4,7 +4,7 @@ const BidButton = ({ openBidModal }) => {
   return (
     <button
       onClick={openBidModal}
-      className='bg-purple-gradient px-6 uppercase font-bold my-12'
+      className='bg-purple-gradient px-6 uppercase my-12 px-6 uppercase py-1 text-md'
     >
       Bid
     </button>

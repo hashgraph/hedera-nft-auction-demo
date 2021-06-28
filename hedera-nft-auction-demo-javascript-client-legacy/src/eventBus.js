dcc9881893f583b353e19044b36ba4182131f245
Greg Scullard <gregscullard@hedera.com>
2021-06-28 15:44:03 +0200

    remove legacy javascript client

--- hedera-nft-auction-demo-javascript-client-legacy/src/eventBus.js
+++
@@ -1,9 +0,0 @@
-import Vue from "vue";
-
-export const EventBus = new Vue();
-
-export const BUSY_EVENT = "busy";
-export const FOOTER_NOTIFICATION = "showMessage";
-export const ERROR_NOTIFICATION = "showError";
-export const MIRROR_SELECTION = "mirrorSelection";
-export const ENV_DATA = "environmentReady";

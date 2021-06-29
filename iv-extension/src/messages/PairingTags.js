04ff464f8ebf3169e81d3ebc3d29652701bceac3
Greg Scullard <gregscullard@hedera.com>
2021-06-29 11:27:32 +0200

    Remove IV Extension

--- iv-extension/src/messages/PairingTags.js
+++
@@ -1,2 +0,0 @@
-export const INJECTED = 'injected';
-export const IV = 'IV';
--- iv-extension/src/models/prompts/PromptTypes.js
+++
@@ -1,3 +0,0 @@
-export const REQUEST_LOGIN = 'requestLogin';
-export const REQUEST_SIGNATURE = 'requestSignature';
-export const REQUEST_ADD_ACCOUNT = 'requestAddAccount';

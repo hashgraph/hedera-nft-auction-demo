99a43f4bcc56618be4950e27841c96cb5f65c113
Greg Scullard <gregscullard@hedera.com>
2021-06-29 12:01:57 +0200

    Remove IV Extension

--- iv-extension/src/util/StringHelpers.js
+++
@@ -1,9 +0,0 @@
-export default class StringHelpers {
-
-    static camelToTitle(camel){
-        return camel
-            .replace(/([A-Z])/g, (match) => ` ${match}`)
-            .replace(/^./, (match) => match.toUpperCase());
-    }
-
-}

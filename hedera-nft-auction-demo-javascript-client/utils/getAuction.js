1c114c6fbdadc35e08bb04c49ba434e7190dcbd9
Greg Scullard <gregscullard@hedera.com>
2021-07-15 10:40:18 +0200

    Use protocol (http or https) from url
    Signed-off-by: Greg Scullard <gregscullard@hedera.com>

--- hedera-nft-auction-demo-javascript-client/utils/getAuction.js
+++ hedera-nft-auction-demo-javascript-client/utils/getAuction.js
@@ -1,5 +1,5 @@
 const fetchAuction = (auctionId) => {
-  let endpoint = process.env.NEXT_PUBLIC_BASE_API_URL ? process.env.NEXT_PUBLIC_BASE_API_URL : "http://".concat(window.location.hostname).concat(":8081");
+  let endpoint = process.env.NEXT_PUBLIC_BASE_API_URL ? process.env.NEXT_PUBLIC_BASE_API_URL : window.location.protocol.concat("//").concat(window.location.hostname).concat(":8081");
   endpoint = endpoint  + `/v1/auctions/${auctionId}`;
   console.log(endpoint);
   return new Promise(async (resolve, reject) => {

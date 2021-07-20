e923587c4ebcd4fb0f96dacf7f7a2efa5875be46
Keegan Thompson <whereiskeegan@gmail.com>
2021-07-20 10:07:30 -0700

    adding in default endpoint
    Signed-off-by: Keegan Thompson <whereiskeegan@gmail.com>

--- hedera-nft-auction-demo-javascript-client/utils/constants.js
+++ hedera-nft-auction-demo-javascript-client/utils/constants.js
@@ -1,2 +1,6 @@
-
-export const API_BASE_URL = process.env.NEXT_PUBLIC_BASE_API_URL
+export const API_BASE_URL =
+  process.env.NEXT_PUBLIC_BASE_API_URL ||
+  window.location.protocol
+    .concat('//')
+    .concat(window.location.hostname)
+    .concat(':8081/v1')

7dcba4928b509672a22940c9fb28842f3d0b763f
Keegan Thompson <whereiskeegan@gmail.com>
2021-07-20 10:25:12 -0700

    removing need for API port env
    Signed-off-by: Keegan Thompson <whereiskeegan@gmail.com>

--- hedera-nft-auction-demo-javascript-client/utils/constants.js
+++ hedera-nft-auction-demo-javascript-client/utils/constants.js
@@ -1,6 +1,4 @@
-export const API_BASE_URL =
-  process.env.NEXT_PUBLIC_BASE_API_URL ||
-  window.location.protocol
-    .concat('//')
-    .concat(window.location.hostname)
-    .concat(':8081/v1')
+export const API_BASE_URL = window.location.protocol
+  .concat('//')
+  .concat(window.location.hostname)
+  .concat(':8081/v1')

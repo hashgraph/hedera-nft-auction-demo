35f88ca9a841d439badcb9285e945265616ada97
Keegan Thompson <whereiskeegan@gmail.com>
2021-07-20 10:44:49 -0700

    bug fix
    Signed-off-by: Keegan Thompson <whereiskeegan@gmail.com>

--- hedera-nft-auction-demo-javascript-client/utils/constants.js
+++ hedera-nft-auction-demo-javascript-client/utils/constants.js
@@ -4,4 +4,4 @@
 //   .concat(window.location.hostname)
 //   .concat(':8081/v1')
 
-export const API_BASE_URL = `https//104.198.67.87:8081/v1`
+export const API_BASE_URL = `https://104.198.67.87:8081/v1`

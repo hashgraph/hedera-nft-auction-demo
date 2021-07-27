b6c3ab0059ebdefd9508b28d65e1c78fd2debdf2
Greg Scullard <gregscullard@hedera.com>
2021-07-27 17:39:10 +0200

    Fix test
    Signed-off-by: Greg Scullard <gregscullard@hedera.com>

--- hedera-nft-auction-demo-java-node/src/test/java/com/hedera/demo/auction/test/unit/app/UtilsTest.java
+++ hedera-nft-auction-demo-java-node/src/test/java/com/hedera/demo/auction/test/unit/app/UtilsTest.java
@@ -4,12 +4,11 @@ import com.hedera.demo.auction.app.Utils;
 import io.vertx.core.http.HttpServerOptions;
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.net.JksOptions;
+import io.vertx.core.net.PemKeyCertOptions;
 import io.vertx.core.net.PfxOptions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class UtilsTest {
 
@@ -79,9 +78,9 @@ public class UtilsTest {
         HttpServerOptions httpServerOptions = Utils.httpServerOptions(config);
 
         assertTrue(httpServerOptions.isSsl());
-        assertNotNull(httpServerOptions.getPfxKeyCertOptions());
-        PfxOptions pfxOptions = httpServerOptions.getPfxKeyCertOptions();
-        assertEquals(certificate, pfxOptions.getPath());
-        assertEquals(key, pfxOptions.getPassword());
+        assertNotNull(httpServerOptions.getPemKeyCertOptions());
+        PemKeyCertOptions pemKeyCertOptions = httpServerOptions.getPemKeyCertOptions();
+        assertEquals(certificate, pemKeyCertOptions.getCertPath());
+        assertEquals(key, pemKeyCertOptions.getKeyPath());
     }
 }

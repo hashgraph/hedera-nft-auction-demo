d57b1a45c217f088657563f8626b7f62c6e8380f
Keegan Thompson <whereiskeegan@gmail.com>
2021-07-28 12:51:25 -0700

    fixing url

--- hedera-nft-auction-demo-javascript-client/utils/constants.js
+++ hedera-nft-auction-demo-javascript-client/utils/constants.js
@@ -1,5 +1,5 @@
 const isDev = process.env.NODE_ENV === 'development'
 const isProd = process.env.NODE_ENV === 'production'
-console.log('IS DEV', isDev)
-console.log('IS Prod', isProd)
-export const API_BASE_URL = 'https://localhost:8081/v1'
+export const API_BASE_URL = isProd
+  ? 'https://localhost:8081/v1'
+  : 'https://104.198.67.87:8080/v1'

9a6cf8cfc096806dd80503c317baf1a828665659
Keegan Thompson <whereiskeegan@gmail.com>
2021-07-28 12:38:49 -0700

    working on API calls

--- hedera-nft-auction-demo-javascript-client/utils/constants.js
+++ hedera-nft-auction-demo-javascript-client/utils/constants.js
@@ -1,7 +1,2 @@
-// Window is not defined here
-// export const API_BASE_URL = window.location.protocol
-//   .concat('//')
-//   .concat(window.location.hostname)
-//   .concat(':8081/v1')
-
-export const API_BASE_URL = `https://104.198.67.87:8081/v1`
+const isDev = process.env.NODE_ENV === 'development'
+export const API_BASE_URL = 'https://localhost:8081/v1'

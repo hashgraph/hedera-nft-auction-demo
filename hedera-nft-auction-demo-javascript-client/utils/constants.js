22347512e779fe74c486be6d53aaa71306162d2d
Keegan Thompson <whereiskeegan@gmail.com>
2021-07-28 12:55:40 -0700

    wrong port
    Signed-off-by: Keegan Thompson <whereiskeegan@gmail.com>

--- hedera-nft-auction-demo-javascript-client/utils/constants.js
+++ hedera-nft-auction-demo-javascript-client/utils/constants.js
@@ -1,5 +1,5 @@
 const isDev = process.env.NODE_ENV === 'development'
 const isProd = process.env.NODE_ENV === 'production'
 export const API_BASE_URL = isProd
-  ? 'https://104.198.67.87:8080/v1'
-  : 'https://104.198.67.87:8080/v1'
+  ? 'https://104.198.67.87:8081/v1'
+  : 'https://104.198.67.87:8081/v1'

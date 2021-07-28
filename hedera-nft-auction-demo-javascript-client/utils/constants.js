4251570bb07fcb924ef01569ebc8f48304743755
Keegan Thompson <whereiskeegan@gmail.com>
2021-07-28 12:43:11 -0700

    apu url things

--- hedera-nft-auction-demo-javascript-client/utils/constants.js
+++ hedera-nft-auction-demo-javascript-client/utils/constants.js
@@ -1,2 +1,5 @@
 const isDev = process.env.NODE_ENV === 'development'
+const isProd = process.env.NODE_ENV === 'production'
+console.log('IS DEV', isDev)
+console.log('IS Prod', isProd)
 export const API_BASE_URL = 'https://localhost:8081/v1'

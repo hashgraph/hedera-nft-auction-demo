769c9da5957ebe67f4615cdce7116e6f771cfd7a
Greg Scullard <gregscullard@hedera.com>
2021-09-10 03:18:32 +0200

    Update scripts in line with docker-compose file changes
    Signed-off-by: Greg Scullard <gregscullard@hedera.com>

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,18 +2,33 @@
 
 if [ $# -eq 0 ]
   then
-    echo "argument for server url required. e.g. https://serverIp:8081/v1"
+    echo "arguments missing. Add compile or image followed by rest api url. (e.g. https://serverIp:8081/v1)"
+    exit
+fi
+
+if [ $# -eq 1 ]
+  then
+    echo "arguments missing. Add compile or image followed by rest api url. (e.g. https://serverIp:8081/v1)"
+    exit
 fi
 
 cd ..
 
-sudo docker pull ghcr.io/hashgraph/hedera-nft-auction-demo/hedera_nft_auction_java_node:latest
-sudo docker pull ghcr.io/hashgraph/hedera-nft-auction-demo/nft-auction-postgres:latest
-sudo docker-compose rm -f
-sudo docker-compose --profile image build --build-arg URL="$1"
-sudo docker-compose --profile image up -d
+if [ "$1" = "compile" ]
+  then
+    echo "running compiled images"
+elif [ "$1" = "image" ]
+  then
+    echo "pulling images from repository"
+else
+    echo "invalid argument, should be compile or image"
+    exit
+fi
 
-echo "Starting log stream in 2s, CTRL+C to stop (will leave containers running)"
+sudo docker-compose -f docker-compose-$1.yaml rm -f
+sudo docker-compose -f docker-compose-$1.yaml build --build-arg url="$2"
+sudo docker-compose -f docker-compose-$1.yaml up --remove-orphans -d
 
-sleep 2
-sudo docker-compose logs -f --tail=10
+echo "Starting log stream, CTRL+C to stop (will leave containers running)"
+
+sudo docker-compose -f docker-compose-$1.yaml logs -f
